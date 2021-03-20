2c938dfdcd8413394c9f18958d046ab1e98f6211
Herve <hmalvina@yahoo.com>
2021-03-20 13:54:11 +0800

    First commit to github

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+use burger_db;
+INSERT INTO burgers (name, devoured) VALUES ('The Lot', false);
+INSERT INTO burgers (name, devoured) VALUES ('Luger Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Le Pigeon Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Cheeseburger', true);
+INSERT INTO burgers (name, devoured) VALUES ('The Ultimate Double Whopper', true);
+INSERT INTO burgers (name), devoured VALUES ('Raw Steak Tartare Burger', false);
